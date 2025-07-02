9535021852e36fda0f79788bfd25c7bfc48b1ba1
Iratishe Reuben Madaki <iratisher@gmail.com>
2025-07-02 07:16:29 -0500

    my first commit

---
+++ lesson1.py
@@ -0,0 +1,23 @@
+print("Welcome to my first python class")
+print("I am so happy")
+
+name = "Iratishe Reuben Madaki."
+
+print("My name is" ,name)
+
+age = "I am 31 years old."
+
+print(name ,age)
+
+print(name ,age ,"Welcome to my first coding class with Sir Ubong.")
+
+
+
+
+
+
+
+
+
+
+
